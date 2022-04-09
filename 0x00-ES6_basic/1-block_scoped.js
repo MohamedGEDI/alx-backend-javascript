c3ac52b616896e85ac8f701f11f9e80a77f357d1
MohameGEDI <gedimohammed@gmail.com>
2022-04-09 09:28:52 +0000

    constant

---
+++ 0x00-ES6_basic/1-block_scoped.js
@@ -0,0 +1,10 @@
+export default function taskBlock(trueOrfalse) {
+  const task = false;
+  const task2 = true;
+
+  if (trueOrfalse) {
+    var task = true;
+    var task2 = false;
+  }
+
+  return [task, task2];
